c54bde8f0d61be3ab9c562431234c9ed2abaee22
Yash Garg <yashygkf@gmail.com>
2023-08-24 18:54:54 +0530

    refactor: getOwner fucntion added

--- foundry-fund-me/src/FundMe.sol
+++ foundry-fund-me/src/FundMe.sol
@@ -12,7 +12,7 @@ contract FundMe {
     mapping(address => uint256) private s_addressToAmountFunded;
     address[] private s_funders;
 
-    address public immutable i_owner;
+    address private immutable i_owner;
     uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
     AggregatorV3Interface private s_priceFeed;
     
@@ -69,4 +69,7 @@ contract FundMe {
         return s_funders[_index];
     }
 
+    function getOwner() public view returns (address) {
+        return i_owner;
+    }
 }
